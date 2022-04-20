2c850dc92b5a7b6ffec347bf37670ccd0861fa4f
dracospitfire <austin3flores@gmail.com>
2022-04-20 18:46:39 +0000

    expanded assignment added new function and new test parameters

--- myModule.py
+++ myModule.py
@@ -5,3 +5,7 @@ def square(number):
 def double(number):
     # This function returns twice the value of a given number
     return number * 2
+    
+def add(a,b):
+    # This function returns the sum of the given numbers
+    return a + b
--- test_myModule.py
+++ test_myModule.py
@@ -1,6 +1,6 @@
 import unittest
 
-from myModule import square, double
+from myModule import square, double, add
 
 class TestSquare(unittest.TestCase): 
     def test1(self): 
@@ -19,5 +19,20 @@ class TestDouble(unittest.TestCase):
             # test when -3.1 is given as input the output is -6.2.
         self.assertEqual(double(0), 0) 
             # test when 0 is given as input the output is 0.
+
+class TestAdd(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(add(2, 4), 6)
+            # test when 2 and 4 are given as input the output must be 6.
+        self.assertEqual(add(0, 0), 0)
+            # test when 0 and 0 are given as input the output must be 0.
+        self.assertEqual(add(2.3, 3.6), 5.9)
+            # test when 2.3 and 3.6 are given as input the output must be 5.9.
+        self.assertEqual(add('hello', "world"), 'helloworld')
+            # test when the strings 'hello' and 'world' are given as input the output must be 'helloworld'.
+        self.assertEqual(add(2.3000, 4.300), 6.6)
+            # test when 2.3000 and 4.300 are given as input the output must be 6.6.
+        self.assertNotEqual(add(-2, -2), 0)
+            # test when -2 and -2 are given as input the output must not be 0.
 
 unittest.main()
